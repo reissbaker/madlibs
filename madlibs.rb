ea2931103b9d8ca565edc170e280c78f58fd4fc6
Matt Baker <matthew.reiss.baker@gmail.com>
2016-06-18 17:26:28 -0700

    Initial madlibs commit

---
+++ madlibs.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+def generate_madlib(path)
+  lines = []
+  memory = {}
+
+  File.foreach(path) do |line|
+    words = line.split(' ').map do |word|
+      parse_word(memory, word)
+    end
+    lines << words.join(' ')
+  end
+
+  lines.join("\n")
+end
+
+def parse_word(memory, word)
+  return word unless word.match(/^\$[A-z]/)
+
+  matches = word.match(/^(\$[A-z]+)(.*)/)
+  key = matches[1]
+  rest = matches[2]
+  "#{get_value(memory, key)}#{rest}"
+end
+
+def get_value(memory, key)
+  if memory.has_key?(key)
+    return memory[key]
+  end
+
+  STDERR.puts("Please enter a value for #{key[1..-1]}")
+  value = STDIN.gets.chomp
+  memory[key] = value
+  value
+end
+
+filepath = ARGV.first
+raise "No path given" if filepath.nil? || filepath.empty?
+
+madlib = generate_madlib(filepath)
+puts madlib
